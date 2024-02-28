cc6f539e730eb631c74f36864570f4f60d61a748
Shumani Simeli <shumanisimeli@gmail.com>
2024-02-28 20:53:47 +0200

    update

--- binary_trees.h
+++ binary_trees.h
@@ -7,20 +7,19 @@
 #include <string.h>
 
 /**
- *  * struct binary_tree_s - Binary tree node
- *   *   
- *    * @n: Integer stored in the node
- *     * @parent: Pointer to the parent node
- *      * @left: Pointer to the left child node
- *       * @right: Pointer to the right child node
- *        */
+ * struct binary_tree_s - Binary tree node   
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 
 struct binary_tree_s
 {
-		int n;
-			struct binary_tree_s *parent;
-				struct binary_tree_s *left;
-					struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
@@ -34,4 +33,5 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 #endif
