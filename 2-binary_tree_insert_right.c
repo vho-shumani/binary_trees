ab73e88b7ab62492b2b2b45f9afd01d1ed18fff1
Shumani Simeli <shumanisimeli@gmail.com>
2024-02-28 17:34:52 +0200

    updated 2-binary_tree_insert_right.c
    mend

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,6 +13,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
+	node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 	{
 		return (NULL);
