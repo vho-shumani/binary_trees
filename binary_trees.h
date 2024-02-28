806d57ae5ae12020fce95936746c741d65cc4698
Shumani Simeli <shumanisimeli@gmail.com>
2024-02-28 20:18:30 +0200

    created 6-binary_tree_preorder.c, updated binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -7,20 +7,20 @@
 #include <string.h>
 
 /**
- * struct binary_tree_s - Binary tree node
- *   
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+ *  * struct binary_tree_s - Binary tree node
+ *   *   
+ *    * @n: Integer stored in the node
+ *     * @parent: Pointer to the parent node
+ *      * @left: Pointer to the left child node
+ *       * @right: Pointer to the right child node
+ *        */
 
 struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+		int n;
+			struct binary_tree_s *parent;
+				struct binary_tree_s *left;
+					struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
@@ -33,4 +33,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 #endif
