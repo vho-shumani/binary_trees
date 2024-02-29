46912ee7658d7752dc67f1e100cab8d59cb04c7e
Shumani Simeli <shumanisimeli@gmail.com>
2024-02-29 11:40:34 +0200

    updated binary_trees.h and created 10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree.
+ * @tree: pointer to the node to measure the depth.
+ * Return: the depth of node.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->parent)
+		depth = 1 + binary_tree_depth(tree->parent);
+	return (depth);
+}
